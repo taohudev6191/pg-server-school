56cacef5dd1c3baf268ad05f79a60905de87edde
taohuDev6191 <120694152+taohudev6191@users.noreply.github.com>
2022-12-23 21:15:05 +0700

    new update

--- models/student.js
+++ models/student.js
@@ -1,32 +1,8 @@
 const mongoose = require("mongoose")
 
 const studentSchema=mongoose.Schema({
-    student_id:{
-        type:String,
-        required:true
-    },
-    sex:{
-        type:String,
-        required:true
-    },
-    fname:{
-        type:String,
-        required:true
-    },
-    lname:{
-        type:String,
-        required:true
-    },
-    age:{
-        type:String,
-        required:true
-    },
-    classroom_id:{
-        type:String,
-        required:true
-    },
-    subjects:{
-        subject_id:Array
+    Student:{
+        type:Array,
     }
 },{timestamps:true})
 
