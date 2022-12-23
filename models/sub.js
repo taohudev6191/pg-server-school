6e2b5b4cca19a5e3214f27876857585f11939b9f
taohuDev6191 <120694152+taohudev6191@users.noreply.github.com>
2022-12-23 18:42:51 +0700

    Sub js

---
+++ models/sub.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+const subSchema=mongoose.Schema({
+    sub:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model("Subject",subjectSchema)
